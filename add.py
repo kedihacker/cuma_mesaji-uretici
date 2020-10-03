178ff67b3ca30b388435b94ef37ada5d4d0ce657
kedi <68711696+kedihacker@users.noreply.github.com>
2020-10-03 18:33:17 +0300

    mesaj parçacığı(telif hakkı alınmış) ekleme özelliği!!!!
    artık mesaj parçacığı ekleyebilirinizin

--- add.py
+++ add.py
@@ -1,13 +1,56 @@
 import json 
 from os import remove
-with open('cumayedek.json','a')as yedek:
-    yedek.truncate(0)
-    with open('cumatest.json','r')as asıl:
-        backup = asıl.read()
-    yedek.write(backup)
+yazılacaklar = {
+
+
+}
+try:   
+    with open('cumayedek.json','a')as yedek:
+        yedek.truncate(0)
+        with open('cumatest.json','r')as asıl:
+            backup = json.load(asıl)
+        yedek.write(json.dumps(backup))
+        yazılacaklar = backup
+except:
+    pass
+print(type(yazılacaklar))
+
+def bilgial(ad):
+    if ad in yazılacaklar:
+        print('girilen tür var!! parti naiy naniy')
+        while True:
+            print(ad)
+            giriş = input('q hariç heryeş yazılacak q çıkış : ')
+            if giriş == 'q':
+                return True
+                
+            else:
+                yazılacaklar[ad].append(giriş)
+    else:
+        giriş = input('Aradığın tür yok abicim. Ekleyeleimli yoksa gerimi atak Girdiğin {} : '.format(ad))
+        if giriş == 'evet : ':
+            yazılacaklar[ad] = []
+            return True
+        else:
+            return False
 
 while True:
-    break
+    giriş = input('girilecek tür yazın : ')
+    if giriş == 'q':
+        break
+    elif giriş == 'database drop':
+        if input('gerçekten ise vallahi billahi yaz : ') == 'vallahi billahi':
+            with open('cumatest.json','r+') as file:
+                file.truncate(0)
+            yazılacaklar = {}
+            print('database droplandı inş yanlışlıkla yapmışşındır amin')
+            break
+    else:
+        if True == bilgial(giriş):
+            print('işlem tamamlandı')
+        else:
+            print('sikinti var')
+
 
 with open('cumatest.json','r+') as file:
     file.truncate(0)
